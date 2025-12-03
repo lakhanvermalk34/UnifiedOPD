c878b786785b4f77d2ee483172de88a3adcc475b
Lakhan Verma <lakhan@example.com>
2025-12-03 19:41:48 +0530

    changes for home screen

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonSearchbar,
+  IonIcon,
+  IonCard,
+  IonCardContent,
+  IonButton,
+  IonAvatar,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab1',
+  standalone: true,
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonSearchbar,
+    IonIcon,
+    IonCard,
+    IonCardContent,
+    IonButton,
+    IonAvatar,
+  ],
 })
-export class Tab1Page {
-  constructor() {}
-}
+export class Tab1Page {}
