c35a7d1e15d2b7952274966227f1fab32248d6b0
Lakhan Verma <lakhan@example.com>
2025-12-03 19:42:47 +0530

    changes latest

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { addIcons } from 'ionicons';
+import { locationOutline, personCircleOutline } from 'ionicons/icons';
+
+addIcons({
+  'location-outline': locationOutline,
+  'person-circle-outline': personCircleOutline
+});
 
 bootstrapApplication(AppComponent, {
   providers: [
