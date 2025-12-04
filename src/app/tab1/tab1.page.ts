6b02305556384415ed3daa03dba9df47ee191f84
Lakhan Verma <lakhan@example.com>
2025-12-04 17:37:34 +0530

    changes for home screen

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import {
   IonHeader,
   IonToolbar,
@@ -11,12 +11,17 @@ import {
   IonButton,
   IonAvatar,
 } from '@ionic/angular/standalone';
+import { register } from 'swiper/element';
+import { addIcons } from 'ionicons';
+import { triangle, ellipse, square, home, calendar, helpBuoyOutline } from 'ionicons/icons';
 
+register();
 @Component({
   selector: 'app-tab1',
   standalone: true,
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     IonHeader,
     IonToolbar,
@@ -30,4 +35,14 @@ import {
     IonAvatar,
   ],
 })
-export class Tab1Page {}
+export class Tab1Page {
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    autoplay: true,
+    loop: true
+  };
+  constructor() {
+    addIcons({ triangle, ellipse, square, home, calendar, helpBuoyOutline });
+  }
+}
